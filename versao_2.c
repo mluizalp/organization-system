2e9961d499a1fc05bd2c6a593c22f2530f3c4f68
mluizalp <100313811+mluizalp@users.noreply.github.com>
2026-06-01 09:48:06 -0300

    versao 2 funcionando belezinha

--- versao_2.c
+++ versao_2.c
@@ -5,6 +5,21 @@
 
 #define MAX_CHAR 100
 
+int VerificarEspacos(char frase[MAX_CHAR]){
+    int Retorno=1;
+
+    for(int i=0; i<strlen(frase); i++){
+        if(frase[i]!=' ' && frase[i]!='\n'){
+            Retorno=0;
+            break;
+        }
+    }
+    if(Retorno==1){
+        printf("\nAtividade invalida. Digite novamente. ");
+    }
+    return Retorno;
+}
+
 int main(){
 
     int flag=1, opcao, i=0, tam;
@@ -13,7 +28,7 @@ int main(){
     int contador=0;//conta as tarefas
     char buscar_ativ[MAX_CHAR];//variavel para ler a atividade buscada
     int numero;//variavel para a movimentacao de atividades
-    FILE *arquivo;
+    FILE *arquivo;//cria o arquivo
     int ok = 1;
 
     while(flag==1){//loop do menu
@@ -33,24 +48,28 @@ int main(){
 
         switch(opcao){
             case 1:
-                printf("===============================");
+                printf("\n===============================");
                 printf("\n     CADASTRO DE ATIVIDADE     ");
                 printf("\n===============================");
                 do{
-                    printf("\n\nNome da atividade(max 100 caracteres): ");
-                    fgets(atividade[i], MAX_CHAR, stdin);//salva do teclado na matriz
+                    int retorno;
+                    do{
+                        printf("\n\nNome da atividade(max 100 caracteres): ");
+                        fgets(atividade[i], MAX_CHAR, stdin);//salva do teclado na matriz
+                        retorno=VerificarEspacos(atividade[i]);
+                    }while(retorno);
                     atividade[i][strcspn(atividade[i], "\n")] = 0;//remove o \n q o fgets salva no final
                     for(int k=0; k<sizeof(atividade[i]); k++){
                         upperAtividade[i][k] = atividade[i][k];
                         upperAtividade[i][k] = toupper(upperAtividade[i][k]);
                     }
                     status[i]=1;//status da atividade smp começa sendo [1] - a fazer
-                    
+
                     printf("Atividade cadastrada com sucesso!");
 
                     i++;
                     contador++;
-                    
+
                     do{
                         printf("\nDeseja cadastrar outra atividade?\n1 - sim\n2 - nao\n");
                         scanf("%d", &ok);
@@ -63,8 +82,8 @@ int main(){
                 }while(ok==1);//repete enqt a pessoa quiser salvar atividade
 
                 break;
-            case 2:// ok? so precisamos ver como iremos trabalhar com o arquivo e status (caso essa se mantenha)
-                printf("===============================");
+            case 2:
+                printf("\n===============================");
                 printf("\n    LISTAGEM DE ATIVIDADES     ");
                 printf("\n===============================");
                 printf("\nA FAZER:");
@@ -88,7 +107,7 @@ int main(){
 
                 break;
             case 3:
-                printf("===============================");
+                printf("\n===============================");
                 printf("\n   MOVIMENTACAO DE ATIVIDADES  ");
                 printf("\n===============================\n");
                 for(int j=0; j<contador; j++){//seguindo a ideia de clara, imprime a lsita de atividades e usuario escolhe numero
@@ -97,22 +116,22 @@ int main(){
                 }
                 printf("\nDigite o numero da atividade que deseja movimentar: ");
                 scanf("%d", &numero);
-               
+
                 do{
                     printf("\nPara qual status deseja tranferi-la?\n");
                     printf("[1] A fazer\n[2] Em andamento\n[3] Concluida\n");
                     scanf("%d", &status[numero]);//salva o status na posicao da atividade escolhida
                     if(status[numero]<1 || status[numero]>3){
                         printf("\nStatus invalido!");
-                    }//testa se o animal n digitou outro numero pro status
+                    }//testa se o usuario n digitou outro numero pro status
                 }while(status[numero]<1 || status[numero]>3);
                 printf("\nTransferencia concluida!");
                 break;
             case 4://ok? (caso essa se mantenha)
-                printf("===============================");
+                printf("\n===============================");
                 printf("\n   QUANTIDADE DE ATIVIDADES    ");
                 printf("\n==============================");
-                
+
                 int Fazer = 0, Andamento = 0, Concluido = 0;
 
                 for(int j = 0; j < contador; j++) {
@@ -123,40 +142,45 @@ int main(){
                     } else if (status[j] == 3) {
                         Concluido++;
                     }
-                }//vai olhar o vetor status e vê os números (classificacao), ai ele vai la e vai somando 
+                }//vai olhar o vetor status e vê os números (classificacao), ai ele vai la e vai somando
 
                 printf("\nA fazer: %d", Fazer);
                 printf("\nEm andamento: %d", Andamento);
                 printf("\nConcluido: %d", Concluido);
                 printf("\nTotal: %d", contador);
- 
+
                 break;
             case 5:
-                printf("===============================");
+                printf("\n===============================");
                 printf("\n      BUSCA DE ATIVIDADE       ");
                 printf("\n===============================");
                 int validador=0, pos;
-                do{ 
+                do{
                     printf("\nDigite o nome da atividade: ");
                     fgets(buscar_ativ, sizeof(buscar_ativ), stdin);//le a atividade q o usuario digitou
                     buscar_ativ[strcspn(buscar_ativ, "\n")] = '\0';//tira o \n
+
                     for (int j = 0; buscar_ativ[j] != '\0'; j++) {
                         buscar_ativ[j] = toupper(buscar_ativ[j]);//deixa tudo maiusculo para a busca
                     }
+
                     for(int j=0; j<contador; j++){
                         if(strcmp(upperAtividade[j], buscar_ativ)==0){//se a string de busca e a original maiuscula forem iguais
                             pos=j;//salva a posicao da atividade encontrada
                             validador=1;//confirma q foi encontrado p terminar a verificacao
                         }
                     }
+
                     if(validador==0){
                         printf("\nAtividade não encontrada.");
                     }
+
                 }while(validador==0);
 
                 printf("\nAtividade encontrada!\n");
                 puts(atividade[pos]);
                 printf("\nStatus - ");
+
                 if(status[pos]==1){
                     printf("A fazer");
                 }else if(status[pos]==2){
@@ -172,15 +196,15 @@ int main(){
         }
     }
 
-    arquivo=fopen("atividades.txt", "a");
+    arquivo=fopen("atividades.txt", "w");//lembrar de mudar parametro
 
     if (arquivo == NULL) {
         printf("Erro ao abrir o arquivo!\n");
-        return 1;   
+        return 1;
     }//caso o fopen falhe
 
-    for(int p=0; p<contador; p++){               
-        fprintf(arquivo,"%d\n%s\n%s", status[p], atividade[p], upperAtividade[p]);//salva o status e a atividade no arquivo
+    for(int p=0; p<contador; p++){
+        fprintf(arquivo,"%d\n%s\n%s\n", status[p], atividade[p], upperAtividade[p]);//salva o status e a atividade no arquivo
     }
 
     fclose(arquivo);
